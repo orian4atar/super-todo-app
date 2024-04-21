05e13d5c5339e1571d7fb6e30df57a442ad73997
Oriana Ruiz <orianaatar@gmail.com>
2024-04-21 20:58:53 -0300

    feat: zustand ultra basic scaffolding.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { create } from 'zustand'
 
 import './index.scss';
 
+interface State {
+  bears: number;
+}
+
+const useStore = create((set) => ({
+  bears: 0,
+  increasePopulation: () => set((state: State) => ({ bears: state.bears + 1 })),
+  removeAllBears: () => set({ bears: 0 }),
+  updateBears: (newBears: number) => set({ bears: newBears }),
+}));
+
+function BearCounter() {
+  const bears = useStore((state) => state.bears)
+  return <h1>{bears} around here...</h1>
+}
+
+function Controls() {
+  const increasePopulation = useStore((state) => state.increasePopulation)
+  return <button onClick={increasePopulation}>one up</button>
+}
+
 const App: React.FC = () => {
-  return <div><h1>Hello, world!</h1></div>;
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <BearCounter />
+      <Controls />
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
