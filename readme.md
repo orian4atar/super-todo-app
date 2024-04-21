3a8ec8cfd4121e2675b34d9b2a9aa5c3a1b80bbd
Oriana Ruiz <orianaatar@gmail.com>
2024-04-21 20:42:12 -0300

    feat: add watch options.

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Getting started
+
+Run `npm i` command.
+
+# Start development server
+
+Run `npm start`command.
+
+# Developing?
+
+Run `npm start` and `npm test` commands.
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const App: React.FC = () => {
-  return <div>Hello, world!</div>;
+  return <div><h1>Hello, world!</h1></div>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
